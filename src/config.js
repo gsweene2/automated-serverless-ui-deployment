8d89c36109042eecd0fdcb08706cc14fd0cfcc9d
gsweene2 <gsweene2@u.rochester.edu>
2019-10-13 02:25:57 -0400

    Update config for newly deployed stacks

--- src/config.js
+++ src/config.js
@@ -1,17 +1,17 @@
 const dev = {
   s3: {
     REGION: "us-east-1",
-    BUCKET: "garretts-notes-service-dev-attachmentsbucket-14sduqcntsgtu"
+    BUCKET: "garretts-notes-service-dev-attachmentsbucket-1jzmjmvqfvl4k"
   },
   apiGateway: {
     REGION: "us-east-1",
-    URL: "https://7drt7fxqvl.execute-api.us-east-1.amazonaws.com/dev"
+    URL: "https://kzm9kjk4tf.execute-api.us-east-1.amazonaws.com/dev"
   },
   cognito: {
     REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_7GF3xorW9",
-    APP_CLIENT_ID: "1nd3bf8h3u2lepoie732tun1hn",
-    IDENTITY_POOL_ID: "us-east-1:b743f651-6ca5-4da4-97b1-6a86cb88504b"
+    USER_POOL_ID: "us-east-1_epBxjRTaC",
+    APP_CLIENT_ID: "6ihrv79e1b2cj62b9e95rcsira",
+    IDENTITY_POOL_ID: "us-east-1:73dd19ab-e60c-46f1-bd32-ec1069016024"
   },
   STRIPE_KEY: "pk_test_jnofTXOrmkOpx2HrUp2AHv2S00yVVcvidx"
 };
@@ -19,17 +19,17 @@ const dev = {
 const prod = {
   s3: {
     REGION: "us-east-1",
-    BUCKET: "garretts-notes-service-prod-attachmentsbucket-45kadg09pe9b"
+    BUCKET: "garretts-notes-service-prod-attachmentsbucket-5pgkroyn8bqq"
   },
   apiGateway: {
     REGION: "us-east-1",
-    URL: "https://2gssvci93k.execute-api.us-east-1.amazonaws.com/prod"
+    URL: "https://nv31c9cyn1.execute-api.us-east-1.amazonaws.com/prod"
   },
   cognito: {
     REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_2IGyFmTdL",
-    APP_CLIENT_ID: "79pjocr53rhet1ra5k0660156v",
-    IDENTITY_POOL_ID: "us-east-1:50cf7e3f-b06b-4a5a-b1a4-460c89bdd8b4"
+    USER_POOL_ID: "us-east-1_RYqNopLsu",
+    APP_CLIENT_ID: "3llmj11blgddq5l4i1bel56tju",
+    IDENTITY_POOL_ID: "us-east-1:66ed5639-cd4e-476a-9a61-fa2c3499ba2c"
   }
 };
 
